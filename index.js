2a0b969e9c3a5c595d726233d2fd0ba83e218c15
tduyngn <68032868+tduyngn@users.noreply.github.com>
2022-01-03 15:34:13 -0500

    kick off pages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var pix = document.getElementsByClassName("pixel");
 
 for (var i = 0; i < pix.length; i++) {
-  pix[i].style.animationDelay = Math.ceil(Math.random()*5000)+"ms";
+  pix[i].style.animationDelay = Math.floor(Math.random()*5000)+"ms";
 }
 
+alert("(PC only) move around to find the dot")
